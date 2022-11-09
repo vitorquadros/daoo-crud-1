4dec66979f1656c74efa23f05fec5c829d1d4295
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-11-09 11:09:14 -0300

    refact: exemplos de novas rotas para controladores

--- aula_03_pdo_mvc/src/routes.php
+++ aula_03_pdo_mvc/src/routes.php
@@ -2,9 +2,15 @@
 
 use Daoo\Aula03\controller\Route;
 use Daoo\Aula03\controller\api\Produto;
+// use Daoo\Aula03\controller\api\Usuario;
 
 Route::routes([
 	'produto' => Produto::class,
+	// 'usuario' => Usuaraio::class
 ]);
+
+//api:
+// composer run api
+// composer run test
 //http://localhost:8081/classe/metodo/parametro
 //http://localhost:8081/produto/
